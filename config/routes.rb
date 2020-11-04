96b8f706503436bb51e85d26c8c7f43b6d5ad549
ChriZoizo <christeufeur83@gmail.com>
2020-11-04 15:12:13 +0100

    petit commit des familles

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   root "event#index"
   resources :users, only: [:show]
   resources :charges
+  resources :attendances
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
